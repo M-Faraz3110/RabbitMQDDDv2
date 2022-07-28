a724d95b8fc3f232c4140d643a51d0fb468df884
M-Faraz3110 <faraz551@live.com>
2022-07-28 12:01:40 +0400

    further changes

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,16 +1,13 @@
 package app
 
 import (
-	"context"
 	v1 "rabbitmqdddv2/pkg/app/controllers/v1"
 	"rabbitmqdddv2/pkg/domain"
 	"rabbitmqdddv2/pkg/infra"
 	"rabbitmqdddv2/pkg/infra/logger"
-	"time"
 
 	trace "github.com/BetaLixT/appInsightsTrace"
 	"github.com/google/wire"
-	"go.uber.org/zap"
 )
 
 var dependencySet = wire.NewSet(
@@ -53,44 +50,15 @@ func (a *app) startService(topic string) {
 	a.v1fcast.Csub(a.lgr.NewLogger(), topic)
 }
 
-func (a *app) traceRequest(
-	context context.Context,
-	method,
-	path,
-	query,
-	agent,
-	ip string,
-	status,
-	bytes int,
-	start,
-	end time.Time) {
-	latency := end.Sub(start)
-
-	lgr := a.lgr.NewLogger()
-	a.insights.TraceRequest(
-		context,
-		method,
-		path,
-		query,
-		status,
-		bytes,
-		ip,
-		agent,
-		start,
-		end,
-		map[string]string{},
-	)
-	lgr.Info(
-		"Request",
-		zap.Int("status", status),
-		zap.String("method", method),
-		zap.String("path", path),
-		zap.String("query", query),
-		zap.String("ip", ip),
-		zap.String("userAgent", agent),
-		zap.Time("mvts", end),
-		zap.String("pmvts", end.Format("2006-01-02T15:04:05-0700")),
-		zap.Duration("latency", latency),
-		zap.String("pLatency", latency.String()),
-	)
-}
+// func (a *app) traceRequest(
+// 	context context.Context,
+// 	method,
+// 	path,
+// 	query,
+// 	agent,
+// 	ip string,
+// 	status,
+// 	bytes int,
+// 	start,
+// 	end time.Time) {
+// }
